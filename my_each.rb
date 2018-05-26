3428fecbe7f3781086af6cc2a6d27955358c2baa
Leticia Molina <letimolina@gmail.com>
2018-05-26 18:21:02 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,6 +3,7 @@ def my_each(collection) # put argument(s) here
     i = 0
     while i < collection.length
       yield(collection[i])
+      puts collection[i]
       i = i + 1
     end
    collection
