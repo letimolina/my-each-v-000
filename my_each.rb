dd49cc90cab1b894e07b86d29b2dc940c4663cec
Leticia Molina <letimolina@gmail.com>
2018-05-26 18:25:26 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(collection) # put argument(s) here 
+  if block_given?
+    i = 0
+    while i < collection.length
+      yield(collection[i])
+      # puts collection[i]
+      i = i + 1
+    end
+   collection
+  else
+    puts "Hey! No block was given!"
+  end
+end  # code here
+# end
+
