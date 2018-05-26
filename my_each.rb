12da38978edc06a13bd6d07f4429995998ef8df8
Leticia Molina <letimolina@gmail.com>
2018-05-26 18:28:51 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,8 @@
-def my_each(collection) # put argument(s) here 
-  if block_given?
+def my_each(collection) 
     i = 0
     while i < collection.length
       yield(collection[i])
-      # puts collection[i]
       i = i + 1
     end
    collection
-  else
-    puts "Hey! No block was given!"
-  end
-end  # code here
-# end
-
+end 
